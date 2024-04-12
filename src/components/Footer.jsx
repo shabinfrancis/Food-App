496ba8bff2afb814cc158187674b049357c34ae8
Shabin Francis <shabinfrancis25@gmail.com>
2024-04-12 07:18:48 +0530

    First Commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,15 @@
+import React from "react"
+import { Link } from "react-router-dom"
+
+export default function Footer() {
+    return (
+        <div className="d-flex flex-column flex-sm-row justify-content-between py-4 my-4 border-top">
+      <p>© 2024 Company, Inc. All rights reserved.</p>
+      <ul className="list-unstyled d-flex">
+        <li className="ms-3"><Link className="link-body-emphasis" to="#"></Link></li>
+        <li className="ms-3"><Link className="link-body-emphasis" to="#"></Link></li>
+        <li className="ms-3"><Link className="link-body-emphasis" to="#"></Link></li>
+      </ul>
+    </div>
+    )
+}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,13 @@
 import Navbar from "../components/Navbar"
 import FoodCard from "../components/FoodCard"
+import Footer from "../components/Footer"
 export default function Home() {
     return(
         <>
           <Navbar />
           <FoodCard />
+          <FoodCard />
+          <Footer />
         </>
     )
 }
