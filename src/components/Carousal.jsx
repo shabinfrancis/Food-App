941d8b38fde5c7d97f3ae2232a62954208a6f408
Shabin Francis <shabinfrancis25@gmail.com>
2024-04-12 08:18:54 +0530

    First Commit

---
+++ src/components/Carousal.jsx
@@ -0,0 +1,34 @@
+import React from "react"
+
+export default function Carousal() {
+    return (
+        <div id="carouselExampleFade" className="carousel slide carousel-fade" data-bs-ride="carousal" style={{objectFit: "contain !important"}}>
+
+            <div className="carousel-inner" id="carousal">
+                <div className="carousal-caption" style={{zIndex:"10"}}>
+                    <form className="d-flex">
+                        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+                        <button className="btn btn-outline-danger" type="submit">Search</button>
+                    </form>
+                </div>
+                <div className="carousel-item active">
+                    <img src="https://source.unsplash.com/random/900x700/?burger" className="d-block w-100" alt="..." />
+                </div>
+                <div className="carousel-item">
+                    <img src="https://source.unsplash.com/random/900x700/?pastry" className="d-block w-100" alt="..." />
+                </div>
+                <div className="carousel-item">
+                    <img src="https://source.unsplash.com/random/900x700/?barbeque" className="d-block w-100" alt="..." />
+                </div>
+            </div>
+            <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="prev">
+                <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span className="visually-hidden">Previous</span>
+            </button>
+            <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="next">
+                <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                <span className="visually-hidden">Next</span>
+            </button>
+        </div>
+    )
+}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import Navbar from "../components/Navbar"
 import FoodCard from "../components/FoodCard"
 import Footer from "../components/Footer"
+import Carousal from "../components/Carousal"
 export default function Home() {
     return(
         <>
           <Navbar />
+          <Carousal />
           <FoodCard />
           <FoodCard />
           <Footer />
